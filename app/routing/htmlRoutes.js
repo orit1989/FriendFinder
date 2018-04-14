cc7742e485de6e85598bd1b69f77a27fc4b7dd8a
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-04-14 17:26:28 -0400

    with html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,4 @@
+
 // We need to include the path package to get the correct file path for our html
 var path = require("path");
 
@@ -5,12 +6,12 @@ module.exports = function(app){
 
 	// In each of the below cases the user is shown an HTML page of content
 	app.get("/survey", function(req, res){
-		res.sendFile(path.join(__dirname + "/.app/public/survey.html"));
+		res.sendFile(path.join(__dirname + "../../public/survey.html"));
 	});
 
 	// If no matching route is found default to home
 	app.use(function(req, res){
-		res.sendFile(path.join(__dirname + "/.app/public/home.html"));
+		res.sendFile(path.join(__dirname + "../../public/home.html"));
 	});
 
 }

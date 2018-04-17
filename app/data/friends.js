a624a849c8857abb886dfefc91199206c7bf3175
Orit Freud <oritfreud@Orits-MacBook-Air.local>
2018-04-17 10:06:59 -0400

    frontend w.i.p

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friendArray = [
     {
-        name: "Johnny",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Genie",
+        photo: "https://img.buzzfeed.com/buzzfeed-static/static/2016-01/21/13/campaign_images/webdr04/how-well-do-you-know-the-genie-from-aladdin-2-5437-1453401463-9_dblbig.jpg",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
-        name: "Mary",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Ariel",
+        photo: "https://lumiere-a.akamaihd.net/v1/images/character_disneyprincess_ariel_262253c9.jpeg?region=0,0,300,300",
         scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
     },
     {
-        name: "Linda",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Belle",
+        photo: "http://cdn.playbuzz.com/cdn/14fbf66e-947a-4d91-ba61-ab72262f36ad/16b16a95-56da-449b-a2ed-faefa51aadc5_560_420.jpg",
         scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
     },
     {
-        name: "Chris",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Charming",
+        photo: "http://images6.fanpop.com/image/photos/37700000/Prince-Charming-disney-37796536-500-421.png",
         scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
     },
     {
-        name: "Austin",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Esmeralda",
+        photo: "http://images4.fanpop.com/image/photos/16400000/Esmeralda-disney-leading-ladies-16434443-500-408.jpg",
         scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
-        name: "Jenny",
-        photo: "https://i.ytimg.com/vi/d8AqywgaQEg/hqdefault.jpg",
+        name: "Aladin",
+        photo: "http://www.ondanaranjacope.com/agenda/imagenes/1482424876.jpg",
         scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
     }
 ]
